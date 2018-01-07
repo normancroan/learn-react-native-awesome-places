940fc85f3ec78f9c1f6d1afc2e51b04c4d8b4569
Norman Croan <ncroan@gmail.com>
2018-01-07 18:25:06 -0500

    handle user input, identical to react

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    placeName: ""
+  }
+
+  onPlaceNameChange = placeName => {
+    this.setState({ placeName })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!!!!!!!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <TextInput style={{height:60, width:300, borderColor: 'gray', borderWidth: 1}} value={this.state.placeName} onChangeText={this.onPlaceNameChange}/>
       </View>
     );
   }

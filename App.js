03e2dcbcac9f8642281d935f3d30f5693f764fa2
Norman Croan <ncroan@gmail.com>
2018-01-07 18:28:23 -0500

    add prettier, format

--- App.js
+++ App.js
@@ -1,29 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default class App extends React.Component {
-  state = {
-    placeName: ""
-  }
+	state = {
+		placeName: ""
+	};
 
-  onPlaceNameChange = placeName => {
-    this.setState({ placeName })
-  }
+	onPlaceNameChange = placeName => {
+		this.setState({ placeName });
+	};
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <TextInput style={{height:60, width:300, borderColor: 'gray', borderWidth: 1}} value={this.state.placeName} onChangeText={this.onPlaceNameChange}/>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<TextInput
+					style={{
+						height: 60,
+						width: 300,
+						borderColor: "gray",
+						borderWidth: 1
+					}}
+					value={this.state.placeName}
+					onChangeText={this.onPlaceNameChange}
+				/>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center"
+	}
 });
